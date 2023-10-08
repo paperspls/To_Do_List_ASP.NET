b570728ddc908579212fc3aa89d706dabb8cf8e1
Rhyan Magalhães <rjobdg@outlook.com>
2023-10-07 21:24:44 -0300

    Create and Config DataBase

--- ToDoListAPI/Program.cs
+++ ToDoListAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoListAPI.Data;
 
 namespace ToDoListAPI
 {
@@ -10,6 +12,15 @@ namespace ToDoListAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            // Conexão com o Banco de dados
+            var connectionString = builder.Configuration.
+                    GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -28,6 +39,15 @@ namespace ToDoListAPI
 
 
             var app = builder.Build();
+
+            // Criar o Banco de dados e as tabelas Automaticamente
+            using (var scope = app.Services.CreateAsyncScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+
+            app.UseDeveloperExceptionPage();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
