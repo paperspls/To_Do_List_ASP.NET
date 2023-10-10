08d0cff4b2a4ebd505439a922ecbc51caf84b5ed
Rhyan Magalhães <rjobdg@outlook.com>
2023-10-10 00:38:38 -0300

    Creating the Docker file

--- ToDoListAPI/Program.cs
+++ ToDoListAPI/Program.cs
@@ -1,5 +1,3 @@
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
 using ToDoListAPI.Data;
 using ToDoListAPI.Model;
 using ToDoListAPI.Security.Implements;
@@ -7,9 +5,14 @@ using ToDoListAPI.Security;
 using ToDoListAPI.Service;
 using ToDoListAPI.Service.Implements;
 using ToDoListAPI.Validator;
+using FluentValidation;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
+using ToDoListAPI.Configuration;
 
 namespace ToDoListAPI
 {
@@ -32,13 +35,32 @@ namespace ToDoListAPI
             );
 
             // Conexão com o Banco de dados
-            var connectionString = builder.Configuration.
-                    GetConnectionString("DefaultConnection");
+            // Conexão com o Banco de dados
+            if (builder.Configuration["Environment:Start"] == "PROD")
+            {
+                // Conexão com o PostgresSQL - Nuvem
 
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(connectionString)
-            );
+                builder.Configuration
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("secrets.json");
 
+                var connectionString = builder.Configuration
+               .GetConnectionString("ProdConnection");
+
+                builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseNpgsql(connectionString)
+                );
+            }
+            else
+            {
+                // Conexão com o SQL Server - Localhost
+                var connectionString = builder.Configuration
+                .GetConnectionString("DefaultConnection");
+
+                builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(connectionString)
+                );
+            }
             // Validação das Entidades
             builder.Services.AddTransient<IValidator<Tarefa>, TarefaValidator>();
             builder.Services.AddTransient<IValidator<Categoria>, CategoriaValidator>();
@@ -72,10 +94,51 @@ namespace ToDoListAPI
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            //Registrar o Swagger
+            builder.Services.AddSwaggerGen(options =>
+            {
+
+                //Personalizar a Págna inicial do Swagger
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "To-Do List",
+                    Description = "Projeto To-Do List - ASP.NET Core 7 - Entity Framework",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Rhyan Magalhaes",
+                        Email = "rhyan.magalhaes@outlook.com",
+                        Url = new Uri("https://github.com/paperspls/To_Do_List_ASP.NET")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Github",
+                        Url = new Uri("https://github.com/paperspls")
+                    }
+                });
+
+                //Adicionar a Segurança no Swagger
+                options.AddSecurityDefinition("JWT", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Digite um Token JWT válido!",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+
+                //Adicionar a configuração visual da Segurança no Swagger
+                options.OperationFilter<AuthResponsesOperationFilter>();
+
+            });
+
+            // Adicionar o Fluent Validation no Swagger
+            builder.Services.AddFluentValidationRulesToSwagger();
 
             // Configuração do CORS
-            builder.Services.AddCors(options => {
+            builder.Services.AddCors(options =>
+            {
                 options.AddPolicy(name: "MyPolicy",
                     policy =>
                     {
@@ -98,11 +161,19 @@ namespace ToDoListAPI
 
             app.UseDeveloperExceptionPage();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            // Swagger Como Página Inicial - Nuvem
+
+            if (app.Environment.IsProduction())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog Pessoal - v1");
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseAuthentication();
