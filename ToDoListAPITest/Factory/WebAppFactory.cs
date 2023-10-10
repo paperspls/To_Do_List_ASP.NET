b25dbf50688eb2d6ea3c842b3c5e7d739a9ba2f5
Rhyan Magalhães <rjobdg@outlook.com>
2023-10-09 23:45:23 -0300

    Created Unitary Tests

---
+++ ToDoListAPITest/Factory/WebAppFactory.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoListAPI;
+using ToDoListAPI.Data;
+using WebMotions.Fake.Authentication.JwtBearer;
+
+namespace ToDoListAPI.Factory
+{
+    public class WebAppFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d =>
+                d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                if (descriptor is not null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryBlogPessoalTest");
+                });
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                using var appContext = scope.ServiceProvider.GetService<AppDbContext>();
+
+                try
+                {
+                    appContext.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    throw;
+                }
+            });
+
+            builder.UseContentRoot(".");
+            builder.UseTestServer().ConfigureTestServices(collection =>
+            {
+                collection.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = FakeJwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = FakeJwtBearerDefaults.AuthenticationScheme;
+                }).AddFakeJwtBearer();
+            });
+
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
